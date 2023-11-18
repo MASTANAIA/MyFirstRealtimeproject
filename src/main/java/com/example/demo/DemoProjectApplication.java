dadc713da3bd1022ec645c10fc7b9e0f8ca6c22a
Srinivasulu Puli <srinivas520881@gmail.com>
2023-11-18 22:51:40 +0530

    commit 2

--- src/main/java/com/example/demo/DemoProjectApplication.java
+++ src/main/java/com/example/demo/DemoProjectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoProjectApplication {
 
 	public static void main(String[] args) {
+		//hello puli
 		SpringApplication.run(DemoProjectApplication.class, args);
 	}
 
